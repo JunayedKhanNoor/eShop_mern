fb3dc05c6a379dc54d85390121768b8cc6c38b69
163002029.junayed <junayednoor@gmail.com>
2022-07-13 12:32:40 +0600

    product schema

---
+++ backend/models/productModel.js
@@ -0,0 +1,66 @@
+import mongoose from 'mongoose';
+const reviewSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    rating: { type: Number, required: true },
+    comment: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+const productSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    name: {
+      type: string,
+      required: true,
+    },
+    image: {
+      type: string,
+      required: true,
+    },
+    brand: {
+      type: string,
+      required: true,
+    },
+    category: {
+      type: string,
+      required: true,
+    },
+    description: {
+      type: string,
+      required: true,
+    },
+    reviews: [reviewSchema],
+    rating: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    numReviews: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    countInStock: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+const Products = mongoose.model('Products', productSchema);
+export default Products;
