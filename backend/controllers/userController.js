54b8c079bc2604e79f5c8f275aa3b490658ff748
163002029.junayed <junayednoor@gmail.com>
2022-08-03 05:59:41 +0600

    user controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -6,6 +6,18 @@ import asyncHandler from 'express-async-handler';
 //@access public
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
-  res.json({ email, password });
+  const user = await User.findOne({ email });
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: null,
+    });
+  } else {
+    res.status(401);
+    throw new Error('Invalid email or Password');
+  }
 });
 export { authUser };
