0b74f902f66a0e67fd66fb505b00fc9ef4dcd8bf
163002029.junayed <junayednoor@gmail.com>
2022-08-06 18:19:14 +0600

    get user profile

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -21,4 +21,21 @@ const authUser = asyncHandler(async (req, res) => {
     throw new Error('Invalid email or Password');
   }
 });
-export { authUser };
+//@desc User Profile
+//@route GET /api/users/profile
+//@access private
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
+export { authUser, getUserProfile };
