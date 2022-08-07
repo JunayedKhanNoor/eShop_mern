dda33a37bfd88ebd5d7c768cb34c14e1d31a797f
163002029.junayed <junayednoor@gmail.com>
2022-08-07 08:46:57 +0600

    API for Register new user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -21,6 +21,34 @@ const authUser = asyncHandler(async (req, res) => {
     throw new Error('Invalid email or Password');
   }
 });
+//@desc Register a new user
+//@route POST /api/users
+//@access public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+  const userExist = await User.findOne({ email });
+  if (userExist) {
+    res.status(400);
+    throw new Error('User already exist');
+  }
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(404);
+    throw new Error('Invalid User Data');
+  }
+});
 //@desc User Profile
 //@route GET /api/users/profile
 //@access private
@@ -38,4 +66,4 @@ const getUserProfile = asyncHandler(async (req, res) => {
     throw new Error('User not found');
   }
 });
-export { authUser, getUserProfile };
+export { authUser, getUserProfile, registerUser };
