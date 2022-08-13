b7332de05136f45ea731ec2a3ce7ecc85c32658b
163002029.junayed <junayednoor@gmail.com>
2022-08-13 15:45:36 +0600

    Shipping screen , save address

---
+++ frontend/src/components/CheckoutSteps.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Nav, NavItem } from 'react-bootstrap';
+
+const CheckoutSteps = ({ step1, step2, step3, step4 }) => {
+  return (
+    <Nav className="justify-content-center mb-4">
+      <NavItem></NavItem>
+    </Nav>
+  );
+};
+
+export default CheckoutSteps;
