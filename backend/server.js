fd9d45d03bb84de1f784a09e0786cfd07f901f9b
163002029.junayed <junayednoor@gmail.com>
2022-08-15 09:20:06 +0600

    Place Order API 🛩️

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,9 @@ import colors from 'colors';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+
 dotenv.config();
 connectDB();
 const app = express();
@@ -14,6 +16,8 @@ app.get('/', (req, res) => {
 });
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
+
 app.use(notFound);
 app.use(errorHandler);
 const PORT = process.env.PORT;
