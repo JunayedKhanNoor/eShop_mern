4d89ff4245cb0254d73638e2095d2f9a63076c6c
163002029.junayed <junayednoor@gmail.com>
2022-08-22 07:57:51 +0600

    Auth middleware for Admin verify 🚔

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -89,4 +89,12 @@ const updateUserProfile = asyncHandler(async (req, res) => {
     throw new Error('User not found');
   }
 });
-export { authUser, getUserProfile, registerUser, updateUserProfile };
+
+//@desc GET all Users Profile
+//@route GET /api/users
+//@access private/admin
+const getUsers = asyncHandler(async (req, res) => {
+  const users = await User.find({});
+  res.json(users);
+});
+export { authUser, getUserProfile, registerUser, updateUserProfile, getUsers };
