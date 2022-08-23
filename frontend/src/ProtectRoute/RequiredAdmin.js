caf51373b65c8be3ccc2e5e068d298396c053e4a
163002029.junayed <junayednoor@gmail.com>
2022-08-23 10:31:06 +0600

    Protect rout for Admin only

--- frontend/src/ProtectRoute/RequiredAdmin.js
+++ frontend/src/ProtectRoute/RequiredAdmin.js
@@ -1,1 +1,22 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Navigate, useLocation } from 'react-router-dom';
+import { logout } from '../actions/userActions';
+import Loader from '../components/Loader';
 
+const RequiredAdmin = ({ children }) => {
+  const dispatch = useDispatch();
+  const userLogin = useSelector((state) => state.userLogin);
+  const { loading, userInfo } = userLogin;
+  const location = useLocation();
+  if (loading) {
+    return <Loader></Loader>;
+  }
+  if (!userInfo.isAdmin) {
+    dispatch(logout());
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
+  return children;
+};
+
+export default RequiredAdmin;
