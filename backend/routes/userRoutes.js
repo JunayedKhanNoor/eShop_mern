d4e055b801293d95a5897e38c71392e738131c3b
163002029.junayed <junayednoor@gmail.com>
2022-08-23 16:37:09 +0600

    User delete API

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import {
   authUser,
+  deleteUser,
   getUserProfile,
   getUsers,
   registerUser,
@@ -14,4 +15,5 @@ router.route('/').post(registerUser).get(protect, admin, getUsers);
 router.post('/login', authUser);
 // router.get('/profile', getUserProfile);
 router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile);
+router.route('/:id').delete(protect, admin, deleteUser);
 export default router;
