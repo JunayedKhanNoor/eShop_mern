e589ddaa107d0bb810c36ad6b3b59ebd26f260c9
163002029.junayed <junayednoor@gmail.com>
2022-08-25 12:11:12 +0600

    Delete Product API

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -21,4 +21,21 @@ const getProductById = asyncHandler(async (req, res) => {
   }
 });
 
+//@desc delete s  product
+//@route DELETE /api/products/:id
+//@access admin/protected
+const deleteProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    // if (req.user._id === product.user._id) {
+    //   //If I want, only the admin who added the product , only he can delete
+    // }
+    await product.remove();
+    res.json({ message: 'Product removed' });
+  } else {
+    res.status(404);
+    throw new Error('Product not found');
+  }
+});
+
 export { getProducts, getProductById, deleteProduct };
